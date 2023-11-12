c095141d8cab3af2291d0349cdc084c7f0b93eb2
rafaelapianezzer <rafaelapianezzer@outlook.com>
2023-11-12 17:56:02 -0300

    "Tarefa 1 concluída"

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,14 @@
-/* Desenvolva sua lógica aqui ... */
+const darkMode = () => {
+    const buttonDark = document.querySelector('.header__btn');
+
+    buttonDark.addEventListener("click", () => {
+        const html = document.querySelector("html");
+        buttonDark.classList.toggle("header__btn--dark-mode")
+
+        html.classList.toggle("dark-mode")
+    })
+
+}
+
+darkMode()
+
